122e8b6f30328848b3d012adbad1a8f55c90f132
SEED264 <seedx264@gmail.com>
2016-07-18 00:56:33 +0900

    Source Upload

---
+++ src/DirectionalBlur.cuh
@@ -0,0 +1,4 @@
+texture<float, 2, cudaReadModeElementType> rtex;
+texture<float, 2, cudaReadModeElementType> gtex;
+texture<float, 2, cudaReadModeElementType> btex;
+texture<float, 2, cudaReadModeElementType> atex;
